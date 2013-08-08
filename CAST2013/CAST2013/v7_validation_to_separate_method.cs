9657e360e8f5e9c7f94a0034f3cdd8ddcb920c0c
Jim Holmes <jim@iterativerose.com>
2013-08-08 19:53:08 -0400

    v7 added.

---
+++ CAST2013/CAST2013/v7_validation_to_separate_method.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace CAST2013
+{
+    [TestFixture]
+    public class v7_validation_to_separate_method
+    { 
+        IWebDriver browser;
+        WebDriverWait wait;
+        IWebElement selectionList;
+
+        [TestFixtureSetUp]
+        public void run_once_before_anything()
+        {
+            var profile = new FirefoxProfile();
+            var exe = new FirefoxBinary(@"D:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+            browser = new FirefoxDriver(exe, profile);
+
+            wait = new WebDriverWait(browser, TimeSpan.FromSeconds(10));
+
+            //browser.Navigate().GoToUrl("http://www.asp.net/ajaxLibrary/AjaxControlToolkitSampleSite/CascadingDropDown/CascadingDropDown.aspx");
+            browser.Navigate().GoToUrl("http://localhost/AJAXDemos/CascadingDropDown/CascadingDropDown.aspx");
+        }
+
+        [TestFixtureTearDown]
+        public void run_once_after_everything_else()
+        {
+            browser.Quit();
+        }
+
+        [Test]
+        public void move_setup_out()
+        {
+            select_menu_item(1, "Acura");
+            select_menu_item(2, "Integra");
+            select_menu_item(3, "Sea Green");
+            assert_correct_message_appears();
+        }
+
+        private void assert_correct_message_appears()
+        {
+            wait.Until(
+                ExpectedConditions.ElementExists(
+                    By.XPath("//span[@id='ctl00_SampleContent_Label1' and text()='You have chosen a Sea Green Acura Integra. Nice car!']")));
+        }
+
+        private void select_menu_item(int listNum, string item)
+        {
+            wait_on_menu_item(listNum, item);
+            selectionList =
+                            browser.FindElement(
+                                By.Id("ctl00_SampleContent_DropDownList" + listNum));
+            var optionsList = new SelectElement(selectionList);
+            optionsList.SelectByText(item);
+        }
+
+        private void wait_on_menu_item(int listNum, string text)
+        {
+            wait.Until(
+                ExpectedConditions.ElementExists(
+                    By.XPath("id('ctl00_SampleContent_DropDownList" + 
+                        listNum+ "')/option[text()='"+
+                        text+"']")));
+        }
+    }
+}
