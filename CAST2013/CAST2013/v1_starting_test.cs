c2f1ea23dde335ba9f27ca923a550f76a0ed8101
Jim Holmes <jim@iterativerose.com>
2013-08-08 03:12:59 -0400

    Initial

---
+++ CAST2013/CAST2013/v1_starting_test.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace CAST2013
+{
+    [TestFixture]
+    public class v1_starting_test
+    {
+        [Test]
+        public void one_method_to_rule_them_all()
+        {
+            var profile = new FirefoxProfile();
+            var exe = new FirefoxBinary(@"D:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe");
+            IWebDriver browser = new FirefoxDriver(exe, profile);
+
+            WebDriverWait wait = new WebDriverWait(browser,TimeSpan.FromSeconds(10));
+
+            browser.Navigate().GoToUrl("http://www.asp.net/ajaxLibrary/AjaxControlToolkitSampleSite/CascadingDropDown/CascadingDropDown.aspx");
+
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("id('ctl00_SampleContent_DropDownList1')/option[text()='Acura']")));
+
+            var selectionList =browser.FindElement(By.Id("ctl00_SampleContent_DropDownList1"));
+            var optionsList = new SelectElement(selectionList);
+            optionsList.SelectByText("Acura");
+
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("id('ctl00_SampleContent_DropDownList2')/option[text()='Integra']")));
+
+            selectionList = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList2"));
+            optionsList = new SelectElement(selectionList);
+            optionsList.SelectByText("Integra");
+
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("id('ctl00_SampleContent_DropDownList3')/option[text()='Sea Green']")));
+
+            selectionList = browser.FindElement(By.Id("ctl00_SampleContent_DropDownList3"));
+            optionsList = new SelectElement(selectionList);
+            optionsList.SelectByText("Sea Green");
+
+            wait.Until(ExpectedConditions.ElementExists(By.XPath("//span[@id='ctl00_SampleContent_Label1' and text()='You have chosen a Sea Green Acura Integra. Nice car!']")));
+
+            browser.Quit();
+        }
+    }
+}
